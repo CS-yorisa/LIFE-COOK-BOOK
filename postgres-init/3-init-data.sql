9f70ccfa8cf7fc0c3ead7d82eab4f31c4ef1fd51
unknown <leejaehee0728@gmail.com>
2024-07-15 19:06:11 +0900

    feat: modify docker-compose, add postgres-init

---
+++ postgres-init/3-init-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password) VALUES
+('John Doe', 'john.doe@example.com', 'password111'),
+('Jane Smith', 'jane.smith@example.com', 'password222'),
+('Alice Johnson', 'alice.johnson@example.com', 'password333');
