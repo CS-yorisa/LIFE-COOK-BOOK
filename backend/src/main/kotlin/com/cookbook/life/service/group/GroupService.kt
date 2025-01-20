d8cbe8d301c71b22dc1e8f93e7c85ccb166adeee
이재희/UCube가입서비스개발팀 <87461594+Hui-Story@users.noreply.github.com>
2025-01-20 23:11:31 +0900

    feat: GroupService CRUD 기능 구현

---
+++ backend/src/main/kotlin/com/cookbook/life/service/group/GroupService.kt
@@ -0,0 +1,15 @@
+package com.cookbook.life.service.group
+
+import com.cookbook.life.model.group.Group
+import com.cookbook.life.model.member.User
+import java.util.*
+
+interface GroupService {
+    fun getGroups(): List<Group>
+    fun getGroup(groupId: UUID): Group
+    fun getUsersByGroup(groupId: UUID): List<User>
+    fun createGroup(groupName: String, limitPerson: Int, groupPassword: String?): Group
+    fun updateGroup(groupId: UUID, groupName: String?, limitPerson: Int?, groupPassword: String?): Group
+    fun joinGroup(groupId: UUID, userId: UUID): String
+    fun leaveGroup(groupId: UUID, userId: UUID): String
+}
